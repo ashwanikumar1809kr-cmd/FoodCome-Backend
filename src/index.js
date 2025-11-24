76acf48960cd133a4c5117ab3b928a7696192e4d
ashwanikumar1809kr-cmd <ashwanikumar1809@gmail.com>
2025-11-25 00:43:12 +0530

    Tested userschema

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 const express = require('express');
-//const bodyParser = require('body-parser');
 
 const serverConfig = require('./config/serverConfig');
 const connectDB = require('./config/dbConfig');
-
+const User = require('./schema/userSchema');
 const app = express();
 
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({extended: true}));
 
-app.post('/ping', (req,res)=> {
+app.post('/ping', (req, res)=> {
     console.log(req.body);
     return res.json({message: "pong"});
 })
@@ -18,4 +17,15 @@ app.post('/ping', (req,res)=> {
 app.listen(serverConfig.PORT, async() => {
     await connectDB();
     console.log(`server started at port ${serverConfig.PORT}...!!`);
+
+ //   const newUser = await User.create ({
+ //       email: 'a@b.com',
+ //       password: '123456',
+ //       firstName: 'jonathan',
+ //       lastName: 'Majors',
+ //       mobileNumber: '1231231230'
+ //   });
+
+ //   console.log("Created new user");
+ //   console.log(newUser);
 });
